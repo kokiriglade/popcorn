a8760b26d6c5fa620bda76b2435f39a427fd7bc8
kokiriglade <60290002+celerry@users.noreply.github.com>
2024-06-23 19:43:39 +0100

    fix <small> tag

--- popcorn/src/main/java/dev/kokiriglade/popcorn/text/minimessage/tag/SmallTag.java
+++ popcorn/src/main/java/dev/kokiriglade/popcorn/text/minimessage/tag/SmallTag.java
@@ -2,12 +2,20 @@ package dev.kokiriglade.popcorn.text.minimessage.tag;
 
 import dev.kokiriglade.popcorn.text.SmallFont;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextComponent;
+import net.kyori.adventure.text.flattener.ComponentFlattener;
 import net.kyori.adventure.text.minimessage.Context;
+import net.kyori.adventure.text.minimessage.internal.parser.node.TagNode;
+import net.kyori.adventure.text.minimessage.internal.parser.node.ValueNode;
+import net.kyori.adventure.text.minimessage.tag.Inserting;
+import net.kyori.adventure.text.minimessage.tag.Modifying;
 import net.kyori.adventure.text.minimessage.tag.Tag;
 import net.kyori.adventure.text.minimessage.tag.resolver.ArgumentQueue;
 import net.kyori.adventure.text.minimessage.tag.resolver.TagResolver;
-import org.bukkit.command.ConsoleCommandSender;
+import net.kyori.adventure.text.minimessage.tree.Node;
+import org.jetbrains.annotations.NotNull;
 
+import java.util.Collections;
 import java.util.Set;
 
 /**
@@ -16,27 +24,60 @@ import java.util.Set;
  * @see SmallFont
  * @since 3.0.0
  */
-class SmallTag {
+class SmallTag implements Modifying {
 
+    private static final ComponentFlattener LENGTH_CALCULATOR = ComponentFlattener.builder()
+        .mapper(TextComponent.class, TextComponent::content)
+        .unknownMapper(x -> "_") // every unknown component gets a single colour
+        .build();
     private static final String SMALL = "small";
     private static final String SM = "sm";
-
     static final TagResolver RESOLVER = TagResolver.resolver(Set.of(SMALL, SM), SmallTag::create);
+    private boolean visited;
+    private int size = 0;
 
     static Tag create(final ArgumentQueue args, final Context ctx) {
-        final String text = args.popOr("null").value();
-
-        /*
-         * the "small caps" font generally looks like crap in the terminal (or at least
-         * it does for me), so avoid using it if we know we're in a terminal environment
-         */
-        if (ctx.target() != null) {
-            if (ctx.target() instanceof ConsoleCommandSender) {
-                return Tag.inserting(Component.text(text));
-            }
-        }
-
-        return Tag.inserting(Component.text(SmallFont.convert(text)));
+        return new SmallTag();
     }
 
+    protected final int size() {
+        return this.size;
+    }
+
+    @Override
+    public void visit(@NotNull Node current, int depth) {
+        if (this.visited) {
+            throw new IllegalStateException("Small tag instances cannot be re-used, return a new one for each resolve");
+        }
+
+        if (current instanceof ValueNode) {
+            final String value = ((ValueNode) current).value();
+            this.size += value.codePointCount(0, value.length());
+        } else if (current instanceof TagNode tag) {
+            if (tag.tag() instanceof Inserting) {
+                // ComponentTransformation.apply() returns the value of the component placeholder
+                LENGTH_CALCULATOR.flatten(((Inserting) tag.tag()).value(), s -> this.size += s.codePointCount(0, s.length()));
+            }
+        }
+    }
+
+    @Override
+    public final void postVisit() {
+        this.visited = true;
+    }
+
+    @Override
+    public Component apply(@NotNull Component current, int depth) {
+        if (current instanceof TextComponent textComponent && ((TextComponent) current).content().length() > 0) {
+            final String content = textComponent.content();
+            current = Component.text(SmallFont.convert(content));
+
+            return current;
+        } else if (!(current instanceof TextComponent)) {
+            final Component ret = current.children(Collections.emptyList());
+            return ret;
+        }
+
+        return Component.empty().mergeStyle(current);
+    }
 }
--- test-plugin/src/main/java/dev/kokiriglade/testplugin/TestPlugin.java
+++ test-plugin/src/main/java/dev/kokiriglade/testplugin/TestPlugin.java
@@ -1,6 +1,7 @@
 package dev.kokiriglade.testplugin;
 
 import com.mojang.brigadier.Command;
+import dev.kokiriglade.popcorn.Popcorn;
 import dev.kokiriglade.popcorn.builder.entity.mob.creature.animal.AxolotlBuilder;
 import dev.kokiriglade.popcorn.builder.item.ItemBuilder;
 import dev.kokiriglade.popcorn.builder.recipe.crafting.ShapedRecipeBuilder;
@@ -24,85 +25,21 @@ public final class TestPlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        Bukkit.addRecipe(
-            ShapedRecipeBuilder
-                .create(
-                    new NamespacedKey(this, "shrink_wand"),
-                    ItemBuilder.ofType(Material.BREEZE_ROD)
-                        .itemName(Component.text("Wand of Shrinking"))
-                        .addFlag(ItemFlag.HIDE_ATTRIBUTES)
-                        .rarity(ItemRarity.RARE)
-                        .addAttributeModifier(
-                            Attribute.GENERIC_SCALE,
-                            new AttributeModifier(
-                                new NamespacedKey(this, "shrink_wand/shrink"),
-                                -0.5d,
-                                AttributeModifier.Operation.ADD_NUMBER,
-                                EquipmentSlotGroup.HAND
-                            )
-                        )
-                        .build()
-                )
-                .shape(" IE", " BI", "B  ")
-                .ingredient('B', new RecipeChoice.MaterialChoice(Material.BLAZE_ROD, Material.BREEZE_ROD))
-                .ingredient('E', new RecipeChoice.MaterialChoice(Material.ENDER_PEARL, Material.ENDER_EYE))
-                .ingredient('I', new RecipeChoice.MaterialChoice(Material.IRON_INGOT))
-                .build()
-        );
-
         getLifecycleManager().registerEventHandler(LifecycleEvents.COMMANDS, event -> {
             Commands commands = event.registrar();
 
             commands.register(
-                Commands.literal("spawn")
-                    .then(Commands.literal("axolotl")
-                        .executes(context -> {
-                            Player player = (Player) context.getSource().getSender();
-
-                            Axolotl axolotl = AxolotlBuilder
-                                .create(player.getLocation().add(0, 1, 0))
-                                .build();
-
-                            player.sendRichMessage("<green>spawned an axolotl</green>");
+                Commands.literal("test")
+                    .executes(context -> {
+                        context.getSource().getSender().sendMessage(
+                            Popcorn.miniMessage().deserialize("Test: <sm>Hello World <red>I am red</red></sm>")
+                        );
+                        context.getSource().getSender().sendMessage(
+                            Popcorn.miniMessage().deserialize("Yo, check out <a:'https://www.youtube.com/watch?v=dQw4w9WgXcQ'>this cool video I just found</a>")
+                        );
 
                             return Command.SINGLE_SUCCESS;
-                        })
-                    )
-                    .requires(commandSourceStack -> commandSourceStack.getSender().isOp() && commandSourceStack.getSender() instanceof Player)
-                    .build()
-            );
-
-            commands.register(
-                Commands.literal("get")
-                    .then(Commands.literal("dirt")
-                        .executes(context -> {
-                            Player player = (Player) context.getSource().getSender();
-                            ItemStack itemStack = ItemBuilder.ofType(Material.DIRT)
-                                .itemName(Component.text("Epic Dirt"))
-                                .rarity(ItemRarity.EPIC)
-                                .addEnchant(Enchantment.UNBREAKING, 1)
-                                .addAttributeModifier(
-                                    Attribute.GENERIC_SCALE,
-                                    new AttributeModifier(
-                                        new NamespacedKey(this, "shrink"),
-                                        -0.5d,
-                                        AttributeModifier.Operation.ADD_NUMBER,
-                                        EquipmentSlotGroup.ANY
-                                    )
-                                )
-                                .build();
-
-                            player.getInventory().addItem(itemStack);
-                            player.sendRichMessage("Gave you [%s]".formatted(
-                                MiniMessage.miniMessage().serialize(
-                                    itemStack.getItemMeta().itemName()
-                                        .hoverEvent(itemStack)
-                                )
-                            ));
-                            return Command.SINGLE_SUCCESS;
-                        })
-                    )
-                    .requires(commandSourceStack -> commandSourceStack.getSender().isOp() && commandSourceStack.getSender() instanceof Player)
+                    })
                     .build()
             );
         });

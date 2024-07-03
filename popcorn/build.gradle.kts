6f7a878ea10c830251d0e4b317c203230e7e25d1
kokiriglade <60290002+kokiriglade@users.noreply.github.com>
2024-07-03 05:03:13 +0100

    Update build.gradle.kts
    Signed-off-by: kokiriglade <60290002+kokiriglade@users.noreply.github.com>

--- popcorn/build.gradle.kts
+++ popcorn/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 group = "dev.kokiriglade"
-version = "3.2.1"
+version = "3.2.2"
 
 paperweight.reobfArtifactConfiguration = io.papermc.paperweight.userdev.ReobfArtifactConfiguration.REOBF_PRODUCTION
 
